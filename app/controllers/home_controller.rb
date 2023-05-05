ba24244cb5538a0d847800548cf5fae6e404720f
tsohleDev <mokhemisitsohle@gmail.com>
2023-05-05 17:35:00 +0200

    add authentication

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
-
     def index
-        render view: 'index'
+        if user_signed_in?
+            redirect_to '/groups'
+        else
+            render view: 'index'
+        end
     end
 end

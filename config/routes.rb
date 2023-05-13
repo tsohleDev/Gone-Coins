e9e543f9fb798a0da642db464d06afbfeaba9aca
tsohleDev <mokhemisitsohle@gmail.com>
2023-05-13 12:54:19 +0200

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   get 'welcome/index'
   devise_for :users
+  
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  resources :groups do
+    resources :transactions
+  end
+  
   # Defines the root path route ("/")
   root "welcome#index"
 end
